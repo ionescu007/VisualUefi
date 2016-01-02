5242547befad70e770829c43236e96040950b894
ionescu007 <aionescu+git@gmail.com>
2016-01-01 19:59:18 -0800

    Add a sample of how to import an EDK2 sample
    So far, VisualEfi came with its own samples. But EDK2 has its own.
    FtdiUsbSerialDxe is an example project file that imports an existing
    EDK2 sample. VsGlue.c is needed for such projects to redirect execution
    to the correct entrypoint name (EDK2's build environment does this on
    its own with special perl/python scripts editing the template source
    files. A simple 2-function file is not the end of the world).

---
+++ samples/FtdiUsbSerialDxe/VsGlue.c
@@ -0,0 +1,42 @@
+//
+// We support unload (but deny it)
+//
+const UINT8 _gDriverUnloadImageCount = 1;
+
+//
+// We require at least UEFI 2.0
+//
+const UINT32 _gUefiDriverRevision = 0x200;
+
+EFI_STATUS
+EFIAPI
+FtdiUsbSerialUnload (
+    IN EFI_HANDLE  ImageHandle
+    );
+
+EFI_STATUS
+EFIAPI
+FtdiUsbSerialEntryPoint (
+    IN EFI_HANDLE        ImageHandle,
+    IN EFI_SYSTEM_TABLE  *SystemTable
+    );
+
+EFI_STATUS
+EFIAPI
+UefiUnload (
+    IN EFI_HANDLE ImageHandle
+    )
+{
+    return FtdiUsbSerialUnload(ImageHandle);
+}
+
+EFI_STATUS
+EFIAPI
+UefiMain (
+    IN EFI_HANDLE ImageHandle,
+    IN EFI_SYSTEM_TABLE *SystemTable
+    )
+{
+    return FtdiUsbSerialEntryPoint(ImageHandle, SystemTable);
+}
+
